910ed27414dac96f08c39f97713391314e2516e1
Sung <Sung@Sung>
2018-06-27 14:59:09 +0900

    First commit

---
+++ src/edu/skku/java/numberstring/Number.java
@@ -0,0 +1,9 @@
+package edu.skku.java.numberstring;
+
+public class Number {
+
+	public static void main(String[] args) {
+		System.out.println(4/2);
+	}
+
+}
---
+++ src/edu/skku/java/numberstring/StringPrac.java
@@ -0,0 +1,9 @@
+package edu.skku.java.numberstring;
+
+public class StringPrac {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
